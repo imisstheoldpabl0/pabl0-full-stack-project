ddd4f9328e4f9327e32e25ad926d1af8456d6471
imisstheoldpabl0 <prubiobusiness@gmail.com>
2024-05-13 15:00:02 +0200

    client/add dashboard chart

--- client/src/components/MainComponent/Dashboard/Chart/Chart.jsx
+++ client/src/components/MainComponent/Dashboard/Chart/Chart.jsx
@@ -1,9 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactApexChart from "react-apexcharts";
 
 const Chart = ({ crypto, amount }) => {
+
+  const [state, setState] = useState({
+    series: [44, 55, 41, 17, 15],
+    options: {
+      chart: {
+        type: 'donut',
+      },
+      responsive: [{
+        breakpoint: 480,
+        options: {
+          chart: {
+            width: 200
+          },
+          legend: {
+            position: 'bottom'
+          }
+        }
+      }]
+    },
+  });
+
+
   return (
     <div>
       <h1>Chart</h1>
+      <ReactApexChart
+        options={state.options}
+        series={state.series} type="donut" />
       <p>{crypto}</p>
       <p>{amount}</p>
     </div>
